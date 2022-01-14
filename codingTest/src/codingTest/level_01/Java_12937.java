984422fdf4833344e379672fb2151463ac307519
raaankim <88372177+raaankim@users.noreply.github.com>
2022-01-15 00:24:40 +0900

    programmers_12937
    정수 num이 짝수일 경우 "Even"을 반환하고 홀수인 경우 "Odd"를 반환하는 함수

---
+++ codingTest/src/codingTest/level_01/Java_12937.java
@@ -0,0 +1,17 @@
+package codingTest.level_01;
+
+public class Java_12937 {
+	class Solution {
+	    public String solution(int num) {
+	        String answer = "";
+	        int n = Math.abs(num);
+	        if(n%2 == 1){
+	            answer = "Odd";
+	        }else {
+	            answer = "Even";
+	        }
+	        return answer;
+	    }
+	}
+
+}
