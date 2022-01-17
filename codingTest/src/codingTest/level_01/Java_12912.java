8fc720a8796e0ce7cf2a0c3adcfbdb1f4e60f63e
raaankim <88372177+raaankim@users.noreply.github.com>
2022-01-17 23:41:16 +0900

    programmers_12912
    두 정수 a, b가 주어졌을 때 a와 b 사이에 속한 모든 정수의 합을 리턴하는 함수, solution을 완성하세요.
    예를 들어 a = 3, b = 5인 경우, 3 + 4 + 5 = 12이므로 12를 리턴합니다.

---
+++ codingTest/src/codingTest/level_01/Java_12912.java
@@ -0,0 +1,16 @@
+package codingTest.level_01;
+
+public class Java_12912 {
+	class Solution {
+	    public long solution(int a, int b) {
+	        long answer = 0;
+	        long min = (long)Math.min(a, b);
+	        long max = (long)Math.max(a, b);
+	        
+	        for(long i=min; i<=max; i++) {
+	            answer += i;
+	        }
+	        return answer;
+	    }
+	}
+}
