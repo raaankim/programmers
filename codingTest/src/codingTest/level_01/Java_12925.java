cfd757e1ff6e8244bdf3d585ef8ac48dbc988a87
raaankim <88372177+raaankim@users.noreply.github.com>
2022-01-28 23:48:53 +0900

    programmers_12925
    문자열 s를 숫자로 변환한 결과를 반환하는 함수, solution을 완성하세요.

---
+++ codingTest/src/codingTest/level_01/Java_12925.java
@@ -0,0 +1,9 @@
+package codingTest.level_01;
+
+public class Java_12925 {
+	public int solution(String s) {
+        int answer = 0;
+        answer = Integer.parseInt(s);
+        return answer;
+    }
+}
